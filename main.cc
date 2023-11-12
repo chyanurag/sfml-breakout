e7743096a118cb2c7ad73f17846ea1b71d1ec9ba
legion <choudharyanuragm@gmail.com>
2023-11-12 18:15:30 +0530

    the game is working now, but there's a bug when the game ends you can't go back to the play screen, need to fix that

--- main.cc
+++ main.cc
@@ -1,5 +1,6 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+#include <string>
 #include <SFML/Audio.hpp>
 #include <iostream>
 
@@ -24,6 +25,7 @@ enum GameState{
 int main(){
 	sf::ContextSettings settings;
 	settings.antialiasingLevel = 4;
+	short int score = 0;
 	sf::RenderWindow window(sf::VideoMode(1920, 1080), "Game", sf::Style::Fullscreen, settings);
 	sf::RectangleShape paddle(sf::Vector2f(300.f, 50.f));
 	sf::CircleShape ball(20.f);
@@ -31,6 +33,33 @@ int main(){
 	enum GameState gameState = Home;
 	bool ended = false;
 	paddle.setPosition(sf::Vector2f(810.f, 1030.f));
+	sf::Font font;
+	if(!font.loadFromFile("GTAV.ttf")){
+		std::cerr << "font not loaded! exiting.." << std::endl;
+		std::exit(1);
+	}
+	sf::Text playText, scoreText;
+	playText.setFont(font);
+	playText.setCharacterSize(50);
+	playText.setString("Press SPACE to play");
+	playText.setFillColor(sf::Color::White);
+	playText.setPosition(800.f, 500.f);
+	scoreText.setFont(font);
+	scoreText.setCharacterSize(40);
+	scoreText.setFillColor(sf::Color::Black + sf::Color::White);
+	scoreText.setPosition(1700.f, 50.f);
+	std::string scoreFont;
+	sf::Text totalScore;
+	totalScore.setFont(font);
+	totalScore.setCharacterSize(50.f);
+	totalScore.setFillColor(sf::Color::White);
+	totalScore.setPosition(700.f, 400.f);
+	sf::Text restartText;
+	restartText.setFont(font);
+	restartText.setString("Press SPACE to restart");
+	restartText.setCharacterSize(50.f);
+	restartText.setFillColor(sf::Color::White);
+	restartText.setPosition(700.f, 500.f);
 	while(window.isOpen()){
 		sf::Event ev;
 		while(window.pollEvent(ev)){
@@ -39,21 +68,41 @@ int main(){
 					window.close();
 					break;
 				case sf::Event::KeyPressed:
-					switch(ev.key.scancode){
-						case sf::Keyboard::Scan::Left:
-							// left key pressed
-							if(paddle.getPosition().x >= 20){
-								float paddlex = paddle.getPosition().x;
-								float paddley = paddle.getPosition().y;
-								paddle.setPosition(paddlex-20, paddley);
+					switch(gameState){
+						case Home:
+							switch(ev.key.scancode){
+								case sf::Keyboard::Scan::Space:
+									gameState = Play;
+									break;
 							}
 							break;
-						case sf::Keyboard::Scan::Right:
-							// right key pressed
-							if(paddle.getPosition().x <= 1900){
-								float paddlex = paddle.getPosition().x;
-								float paddley = paddle.getPosition().y;
-								paddle.setPosition(paddlex+20, paddley);
+						case Play:
+							switch(ev.key.scancode){
+								case sf::Keyboard::Scan::Left:
+									// left key pressed
+									if(paddle.getPosition().x >= 20){
+										float paddlex = paddle.getPosition().x;
+										float paddley = paddle.getPosition().y;
+										paddle.setPosition(paddlex-20, paddley);
+									}
+									break;
+								case sf::Keyboard::Scan::Right:
+									// right key pressed
+									if(paddle.getPosition().x <= 1900){
+										float paddlex = paddle.getPosition().x;
+										float paddley = paddle.getPosition().y;
+										paddle.setPosition(paddlex+20, paddley);
+									}
+									break;
+							}
+							break;
+						case Ended:
+							switch(ev.key.scancode){
+								case sf::Keyboard::Scan::Space:
+									gameState = Home;
+									score = 0;
+									window.clear();
+									continue;
 							}
 							break;
 					}
@@ -61,18 +110,43 @@ int main(){
 			}
 		}
 		window.clear();
-		window.draw(paddle);
-		window.draw(ball);
-		if(checkCollision(ball.getPosition(), paddle.getPosition())){
-			ballVelocity.y > 0 ? ballVelocity.y = -ballVelocity.y : NULL;
+		switch(gameState){
+			case Home:
+				window.draw(playText);
+				break;
+			case Play:
+				window.clear();
+				window.draw(paddle);
+				window.draw(ball);
+				scoreFont = "Score : ";
+				scoreFont.append(std::to_string(score));
+				scoreText.setString(scoreFont);
+				window.draw(scoreText);
+				if(checkCollision(ball.getPosition(), paddle.getPosition())){
+					ballVelocity.y > 0 ? ballVelocity.y = -ballVelocity.y : NULL;
+					score++;
+					ballVelocity.x += .5f;
+					ballVelocity.y += .5f;
+				}
+				if(ball.getPosition().x < 0 || ball.getPosition().x > 1900){
+					ballVelocity.x = -ballVelocity.x;
+				}
+				if(ball.getPosition().y < 0){
+					ballVelocity.y = -ballVelocity.y;
+				}
+				if(ball.getPosition().y > 1080){
+					gameState = Ended;
+					std::string total = "Your total score was : ";
+					total.append(std::to_string(score));
+					totalScore.setString(total);
+				}
+				ball.setPosition(ball.getPosition() + ballVelocity);
+				break;
+			case Ended:
+				window.draw(restartText);
+				window.draw(totalScore);
+				break;
 		}
-		if(ball.getPosition().x < 0 || ball.getPosition().x > 1900){
-			ballVelocity.x = -ballVelocity.x;
-		}
-		if(ball.getPosition().y < 0){
-			ballVelocity.y = -ballVelocity.y;
-		}
-		ball.setPosition(ball.getPosition() + ballVelocity);
 		window.display();
 	}
 	return 0;
